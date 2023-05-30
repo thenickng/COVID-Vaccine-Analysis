268bff75b6f4f75550ad3f757af2ab1598aa8bd8
ssshenjy <ssshenjy@126.com>
2023-05-29 21:58:52 -0700

    R set up change

--- ui.R
+++ ui.R
@@ -7,32 +7,32 @@ state_vacc <- read.csv("us_state_vaccinations.csv", stringsAsFactors = FALSE)
 
 # Introductory page
 intro_tab <- tabPanel(
-  
+  "Introductory Page"
 )
 
 # Visualization 1
 viz1_tab <- tabPanel(
-  
+  "Visualization1 Page"
 )
 
 
 # Visualization 2
 viz2_tab <- tabPanel(
-  
+  "Visualization2 Page"
 )
 
 
 
 # Visualization 3
 viz3_tab <- tabPanel(
-  
+  "Visualization3 Page"
 )
 
 
 
 # Conclusion Page
 conclusion_tab <- tabPanel(
-  
+  "Conclusion Page"
 )
 
 
