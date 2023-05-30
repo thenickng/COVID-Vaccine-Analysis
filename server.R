9cb4145877b90721039bf8ddcf3e38276fb6941a
ssshenjy <ssshenjy@126.com>
2023-05-29 22:11:57 -0700

    Basic set up change and Part of Viz3

--- server.R
+++ server.R
@@ -7,5 +7,27 @@ state_vacc <- read.csv("us_state_vaccinations.csv", stringsAsFactors = FALSE)
 
 
 server <- function(input, output) {
+  # Intro Part
   
+  
+  
+  # Viz1 Part
+  
+  
+  
+  # Viz2 Part
+  
+  
+  
+  
+  # Viz3 Part
+  output$state_total_vacc <- renderPlotly({
+    
+  })
+  
+  
+  
+  
+  
+  # Conclusion Part
 }
--- ui.R
+++ ui.R
@@ -25,7 +25,30 @@ viz2_tab <- tabPanel(
 
 # Visualization 3
 viz3_tab <- tabPanel(
-  "Visualization3 Page"
+  "Visualization3 Page",
+  h1("Total Vaccinations (COVID-19 doses) administered from 2021 to 2023 for Selected States"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput(
+        inputId = "user_select_year", label = h3("Years:"),
+        min = min(state_vacc$year),
+        max = max(state_vacc$year),
+        value = c(min(state_vacc$year), max(state_vacc$year)), 
+        step = 1
+      ),
+      selectInput(
+        inputId = "user_select_country", label = h3("Select Countries"),
+        choices = state_vacc$state,
+        # need to change !!!!!!!!!!!!!!!!!!!1 choices
+        selected = "Washington",
+        multiple = TRUE
+      )
+    ),
+    mainPanel(
+      plotlyOutput("state_total_vacc"),
+      textOutput("plot_description")
+    )
+  )
 )
 
 
